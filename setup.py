3ce895edfd8131da0c2f6c4bfa8d7de6c441b2f8
Jim Fulton <jim@zope.com>
2008-02-07 18:12:27 +0000

    Set to next version #

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ open('doc.txt', 'w').write(long_description)
 
 setup(
     name = 'zc.beforestorage',
-    version = '0.1.0',
+    version = '0.2dev',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = 'View storage before a given time',
