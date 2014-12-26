289658f68133eb40439cb6dc1d1a3e7b976dcfa0
Marius Gedminas <marius@gedmin.as>
2014-12-26 13:57:34 +0200

    PEP-8

--- setup.py
+++ setup.py
@@ -16,41 +16,43 @@ name, version = 'zc.beforestorage', '0'
 import os
 from setuptools import setup, find_packages
 
+
 def read(rname):
     return open(os.path.join(os.path.dirname(__file__), *rname.split('/')
                              )).read()
+
 
 entry_points = """
 """
 
 long_description = (
-        read('src/zc/beforestorage/README.txt')
-        + '\n' +
-        'Download\n'
-        '========\n'
-        )
+    read('src/zc/beforestorage/README.txt')
+    + '\n' +
+    'Download\n'
+    '========\n'
+)
 
 tests_require = ['zope.testing']
 
 setup(
-    name = name,
-    version = version,
-    author = 'Jim Fulton',
-    author_email = 'jim@zope.com',
-    description = 'View storage before a given time',
+    name=name,
+    version=version,
+    author='Jim Fulton',
+    author_email='jim@zope.com',
+    description='View storage before a given time',
     long_description=long_description,
-    license = 'ZPL 2.1',
+    license='ZPL 2.1',
 
-    include_package_data = True,
-    packages = find_packages('src'),
-    namespace_packages = ['zc'],
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools', 'ZODB'],
-    zip_safe = False,
+    include_package_data=True,
+    packages=find_packages('src'),
+    namespace_packages=['zc'],
+    package_dir={'': 'src'},
+    install_requires=['setuptools', 'ZODB'],
+    zip_safe=False,
     entry_points=entry_points,
-    tests_require = tests_require,
-    extras_require = dict(test=tests_require),
-    classifiers = [
+    tests_require=tests_require,
+    extras_require=dict(test=tests_require),
+    classifiers=[
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
@@ -61,5 +63,5 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: Unix',
         'Framework :: ZODB',
-        ],
-    )
+    ],
+)
